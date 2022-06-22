e1504371cd88f36cff13882ab9cfb4117d4f4d10
Pablo Suntaxi <pbsun.ale@gmail.com>
2022-06-21 19:52:08 -0500

    Unidad 2

---
+++ src/main/java/com/uce/edu/demo/ProyectoU2PsApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2PsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2PsApplication.class, args);
+	}
+
+}
